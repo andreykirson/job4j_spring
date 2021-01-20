7a5711dc7207a94b9577cdcccf24939b4306d6fd
fruit <a.s.kirson@gmail.com>
2021-01-20 22:24:26 +1000

    1. Spring DI. Запуск [#364884]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
-        Context context = new Context();
-        context.reg(ConsoleInput.class);
-        context.reg(StartUI.class);
-        StartUI ui = context.get(StartUI.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
         ui.askStr();
     }
 }
